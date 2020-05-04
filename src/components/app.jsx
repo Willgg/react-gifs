9858e7c266ff241820f1a5ae59c1425d1df0cb4b
William Godard <grenier.godard@gmail.com>
2020-05-04 20:13:00 +0200

    end the challenge

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
+
+const GIPHY_API_KEY = '5qSrzOsMT0SHzaK43J36N6wezRoOX0YI';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: 'oDLDbBgf0dkis',
+    }
+    this.searchFunction('dog');
+  }
+
+  searchFunction = (query) => {
+    giphy(GIPHY_API_KEY).search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
   render () {
     return (
       <div>
-        <div className="left-scene"></div>
-        <div className="right-scene"></div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.searchFunction}/>
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId}/>
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
       </div>
     )
   }
